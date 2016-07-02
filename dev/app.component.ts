6e019d3449c91e647a4d0d0ba98d71d0acdd7307
Robeszpierre <robeszpierre@gmail.com>
2016-07-02 17:55:25 +0200

    attribute-directive

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,11 +1,13 @@
 import {Component} from '@angular/core';
+import {AttributeDirectives} from "./attribute-directives.component";
 
 @Component({
     selector: 'my-app',
     template: `
+        <my-attribute-directives></my-attribute-directives>
     `,
-  })
-
+    directives: [AttributeDirectives]
+})
 export class AppComponent {
 
 }
---
+++ dev/highlight.directive.ts
@@ -0,0 +1,17 @@
+import {Directive, ElementRef, OnInit, Renderer} from '@angular/core'
+
+@Directive({
+    selector: '[myHighlight]'
+})
+export class HighlightDirective implements OnInit {
+    private _defaultColor = 'green';
+
+    constructor(private _elRef: ElementRef, private _renderer: Renderer) {
+
+    }
+
+    ngOnInit():any {
+        this._renderer.setElementStyle(this._elRef.nativeElement, 'background-color', this._defaultColor);
+    }
+
+}
